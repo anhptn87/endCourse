5bd0c4130bc21bd75986025d4962fe077eff6c72
anhptn <testeracc87@gmail.com>
2019-01-21 10:55:38 +0700

    delete unused class

--- src/test/java/vt/endCourse/DefinitionTestSuite.java
+++ src/test/java/vt/endCourse/DefinitionTestSuite.java
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = "src/test/resources/features"
-, tags = {"~@Dictinary" ,"@SendNews", "@CreateNews", "@Subscriber"})
+, tags = {"@SendNews", "@CreateNews", "@Subscriber"})
 public class DefinitionTestSuite {
 }
